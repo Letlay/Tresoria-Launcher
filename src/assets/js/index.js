1c3273b8ae6e96fc5ba179bff64fc2b6af682a39
Letlay <letlay.yt@hotmail.com>
2022-07-19 11:52:37 +0200

    Message de début bogoss

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,13 +22,14 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "/play galaxy brain meme"},
+            { "message": "/Salut c'est ninho earape"},
+            { "message": "«/warp cité» Hugo"},
+            { "message": "«Tresoria appartient à iFinity Project"},
+            { "message": "Val Dajol > toi"}
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
-        this.splashAuthor.children[0].textContent = "@" + splash.author;
         await sleep(100);
         document.querySelector("#splash").style.display = "block";
         await sleep(500);
